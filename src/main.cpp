e90682f2e2392d92daa785f86e019629963a9f83
Pierce Oxley <95258151+Parcley27@users.noreply.github.com>
2024-05-10 17:31:55 -0700

    refactor: simplify motor control

--- src/main.cpp
+++ src/main.cpp
@@ -83,27 +83,36 @@ void usercontrol(void) {
     FrontLeft.spin(forward, FrontLeftSpeed, pct);
 
     if (Controller1.ButtonL1.pressing()) {
-      FirstStage.setVelocity(50.0, pct);
+      FirstStage.spin(forward, 50.0, pct);
+
     } else if (Controller1.ButtonL2.pressing()) {
-      FirstStage.setVelocity(-15.0, pct);
+      FirstStage.spin(forward, -15.0, pct);
+
     } else {
-      FirstStage.setVelocity(0.0, pct);
+      FirstStage.stop();
+
     }
 
     if (Controller1.ButtonR1.pressing()) {
-      SecondStage.setVelocity(50.0, pct);
+      SecondStage.spin(forward, 50.0, pct);
+
     } else if (Controller1.ButtonR2.pressing()) {
-      SecondStage.setVelocity(-50.0, pct);
+      SecondStage.spin(forward, -50.0, pct);
+
     } else {
-      SecondStage.setVelocity(0.0, pct);
+      SecondStage.stop();
+
     }
 
-    if (Controller1.ButtonA.pressing()) {
-      Claw.setVelocity(50.0, pct);
-    } else if (Controller1.ButtonB.pressing()) {
-      Claw.setVelocity(-50.0, pct);
+    if (Controller1.ButtonB.pressing()) {
+      Claw.spin(forward, 50.0, pct);
+
+    } else if (Controller1.ButtonA.pressing()) {
+      Claw.spin(forward, -50.0, pct);
+
     } else {
-      Claw.setVelocity(0.0, pct);
+      Claw.stop();
+
     }
 
     if (Controller1.ButtonDown.pressing()) {
